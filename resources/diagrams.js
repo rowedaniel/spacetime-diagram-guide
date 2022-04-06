e285cdf3228228897a6fac890219a00162df81ef
Daniel <rowedanielp@gmail.com>
2022-04-05 23:18:41 -0700

    last minute updates

--- resources/diagrams.js
+++ resources/diagrams.js
@@ -1,9 +1,16 @@
-let currentPoint = 0;
+
+let lastX = [0,0];
+let lastCt = [0,0];
+let currentPoint = -1;
+let lastK = [0,0];
 
 let v = -0.5;
 function updateV() {
     v = document.getElementById("v-slider").value;
     document.getElementById("v-display").innerHTML = v;
+    for(let i=0; i<lastX.length; ++i) {
+        updatePoints(lastX[i], lastCt[i], i, lastK[currentPoint]);
+    }
 }
 
 function updatePoints(x,ct, i, isK)
@@ -20,16 +27,40 @@ function updatePoints(x,ct, i, isK)
     placePoint(aPoints[i], x, ct);
     updatePoint(bPoints[i], x, ct, v, isK);
 
-    let xP, ctP
+    const o_pt = (currentPoint+1)%2;
+    let w=-v;
     if(isK) {
-        [xP, ctP] = getCoords(x,ct, v);
-    } else {
-        [xP, ctP] = getCoords(x,ct, -v);
+        w *= -1;
     }
-    document.getElementById("len-display").innerHTML = xP.toFixed(2);
-    document.getElementById("time-display").innerHTML = ctP.toFixed(2);
-    document.getElementById("spacetime-display-1").innerHTML = (ct*ct - xP*xP).toFixed(2);
-    document.getElementById("spacetime-display-2").innerHTML = (ctP*ctP - xP*xP).toFixed(2);
+    let Ax, Act, Bx, Bct;
+    let AxP, ActP, BxP, BctP;
+
+    [Ax, Act]  = [x, ct];
+    [AxP,ActP] = getCoords(x,ct, w);
+
+    [Bx,Bct]   = [lastX[o_pt],lastCt[o_pt]];
+    [BxP,BctP] = getCoords(lastX[o_pt],lastCt[o_pt], w);
+
+    x_diff = -Ax+Bx;
+    ct_diff = -Act+Bct;
+
+    xP_diff = -AxP+BxP;
+    ctP_diff = -ActP+BctP;
+
+    if(isK) {
+        let tmp = [x_diff, ct_diff];
+        [x_diff, ct_diff] = [xP_diff, ctP_diff];
+        [xP_diff, ctP_diff] = tmp;
+    }
+    
+
+    document.getElementById("len-display").innerHTML = x_diff.toFixed(2);
+    document.getElementById("time-display").innerHTML = ct_diff.toFixed(2);
+    document.getElementById("spacetime-display-1").innerHTML = (ct_diff*ct_diff - x_diff*x_diff).toFixed(2);
+
+    document.getElementById("len-display-p").innerHTML = xP_diff.toFixed(2);
+    document.getElementById("time-display-p").innerHTML = ctP_diff.toFixed(2);
+    document.getElementById("spacetime-display-2").innerHTML = (ctP_diff*ctP_diff - xP_diff*xP_diff).toFixed(2);
 }
 
 window.addEventListener("DOMContentLoaded", function(event) {
@@ -74,8 +105,20 @@ window.addEventListener("DOMContentLoaded", function(event) {
             let x = (event.clientX - rect.left - GRID_SIZE) / width;
             let ct = 1 - (-GRID_SIZE + event.clientY - rect.top) / height;
 
-            updatePoints(x, ct, currentPoint, isK);
             currentPoint = (currentPoint + 1) % 2;
+            lastX[currentPoint] = x;
+            lastCt[currentPoint] = ct;
+            lastK[currentPoint] = isK;
+            const opt = (currentPoint+1)%2;
+            if(isK != lastK[opt]) {
+                let w = -v;
+                if(!isK) { w *= -1; }
+                // flip the other point too, so both are either K or K'
+                [lastX[opt], lastCt[opt]] = getCoords(lastX[opt], lastCt[opt], w);
+            }
+
+            updatePoints(x, ct, currentPoint, isK);
+
 
         });
     }
